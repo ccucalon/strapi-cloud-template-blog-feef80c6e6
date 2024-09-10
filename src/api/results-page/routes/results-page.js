5436730d8347c712cf75b190a9c26bf60e4fec03
ccucalon <ccucalon14@gmail.com>
2024-09-09 23:16:29 -0500

    Results Page Removed

--- src/api/results-page/routes/results-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * results-page router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::results-page.results-page');
